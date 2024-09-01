6d42d55da1ced0d397d460344a8961edaa0d7406
Proddy <josh@zhbot.org>
2024-09-01 22:36:46 +0100

    Fixed `IntersectMesh`
    - Added `NumMeshes`

--- NetP3DLib/NetP3DLib/P3D/Chunks/IntersectMeshChunk.cs
+++ NetP3DLib/NetP3DLib/P3D/Chunks/IntersectMeshChunk.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace NetP3DLib.P3D.Chunks;
 
 [ChunkAttributes((uint)ChunkIdentifier.Intersect_Mesh)]
 public class IntersectMeshChunk : NamedChunk
 {
+    public uint NumMeshes => (uint)Children.Where(x => x.ID == (uint)ChunkIdentifier.Intersect_Mesh_2).Count();
+
     public override byte[] DataBytes
     {
         get
@@ -13,15 +17,18 @@ public class IntersectMeshChunk : NamedChunk
             List<byte> data = [];
 
             data.AddRange(BinaryExtensions.GetP3DStringBytes(Name));
+            data.AddRange(BitConverter.GetBytes(NumMeshes));
 
             return [.. data];
         }
     }
-    public override uint DataLength => (uint)BinaryExtensions.GetP3DStringBytes(Name).Length;
+    public override uint DataLength => (uint)BinaryExtensions.GetP3DStringBytes(Name).Length + sizeof(uint);
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "We want to read the value to progress the BinaryReader, but not set the value anywhere because it's calculated dynamically.")]
     public IntersectMeshChunk(BinaryReader br) : base((uint)ChunkIdentifier.Intersect_Mesh)
     {
         Name = br.ReadP3DString();
+        var numMeshes = br.ReadUInt32();
     }
 
     public IntersectMeshChunk(string name) : base((uint)ChunkIdentifier.Intersect_Mesh)
