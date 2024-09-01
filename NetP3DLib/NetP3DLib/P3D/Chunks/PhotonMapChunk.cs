ae5f1d3f8f3c34d28933925959092b810302e0f2
Proddy <josh@zhbot.org>
2024-09-01 22:51:22 +0100

    Add missing chunks
    - Added `PhotonMap`
    - Added `SmartProp`

---
+++ NetP3DLib/NetP3DLib/P3D/Chunks/PhotonMapChunk.cs
@@ -0,0 +1,244 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace NetP3DLib.P3D.Chunks;
+
+[ChunkAttributes((uint)ChunkIdentifier.Photon_Map)]
+public class PhotonMapChunk : NamedChunk
+{
+    public uint Version { get; set; }
+    public uint NumLights
+    {
+        get => (uint)Lights.Count;
+        set
+        {
+            if (value == NumLights)
+                return;
+
+            if (value < NumLights)
+            {
+                while (NumLights > value)
+                    Lights.RemoveAt(Lights.Count - 1);
+            }
+            else
+            {
+                while (NumLights < value)
+                    Lights.Add(default);
+            }
+            NumLightScales = value;
+        }
+    }
+    public List<string> Lights { get; } = [];
+    public uint NumLightScales
+    {
+        get => (uint)LightScales.Count;
+        set
+        {
+            if (value == NumLightScales)
+                return;
+
+            if (value < NumLightScales)
+            {
+                while (NumLightScales > value)
+                    LightScales.RemoveAt(LightScales.Count - 1);
+            }
+            else
+            {
+                while (NumLightScales < value)
+                    LightScales.Add(default);
+            }
+            NumLights = value;
+        }
+    }
+    public List<float> LightScales { get; } = [];
+    public uint NumPhotons
+    {
+        get => (uint)Photons.Count;
+        set
+        {
+            if (value == NumPhotons)
+                return;
+
+            if (value < NumPhotons)
+            {
+                while (NumPhotons > value)
+                    Photons.RemoveAt(Photons.Count - 1);
+            }
+            else
+            {
+                while (NumPhotons < value)
+                    Photons.Add(new());
+            }
+        }
+    }
+    public List<Photon> Photons { get; } = [];
+
+    public override byte[] DataBytes
+    {
+        get
+        {
+            List<byte> data = [];
+
+            data.AddRange(BinaryExtensions.GetP3DStringBytes(Name));
+            data.AddRange(BitConverter.GetBytes(Version));
+            data.AddRange(BitConverter.GetBytes(NumLights));
+            foreach (var light in Lights)
+                data.AddRange(BinaryExtensions.GetP3DStringBytes(light));
+            foreach (var lightScale in LightScales)
+                data.AddRange(BitConverter.GetBytes(lightScale));
+            data.AddRange(BitConverter.GetBytes(NumPhotons));
+            foreach (var photon in Photons)
+                data.AddRange(photon.DataBytes);
+
+            return [.. data];
+        }
+    }
+    public override uint DataLength => (uint)BinaryExtensions.GetP3DStringBytes(Name).Length + sizeof(uint) + sizeof(uint) + (uint)Lights.Sum(x => BinaryExtensions.GetP3DStringBytes(x).Length) + sizeof(float) * NumLights + sizeof(uint) + (uint)Photons.Sum(x => x.DataBytes.Length);
+
+    public PhotonMapChunk(BinaryReader br) : base((uint)ChunkIdentifier.Photon_Map)
+    {
+        Name = br.ReadP3DString();
+        Version = br.ReadUInt32();
+        var numLights = br.ReadInt32();
+        Lights.Capacity = numLights;
+        LightScales.Capacity = numLights;
+        for (var i = 0; i < numLights; i++)
+            Lights.Add(br.ReadP3DString());
+        for (var i = 0; i < numLights; i++)
+            LightScales.Add(br.ReadSingle());
+        var numPhotons = br.ReadInt32();
+        Photons.Capacity = numPhotons;
+        for (var i = 0; i < numPhotons; i++)
+            Photons.Add(new(br));
+    }
+
+    public PhotonMapChunk(string name, uint version, IList<string> lights, IList<float> lightScales, IList<Photon> photons) : base((uint)ChunkIdentifier.Photon_Map)
+    {
+        Name = name;
+        Version = version;
+        Lights.AddRange(lights);
+        LightScales.AddRange(lightScales);
+        Photons.AddRange(photons);
+    }
+
+    public override void Validate()
+    {
+        if (Lights.Count != LightScales.Count)
+            throw new InvalidDataException($"{nameof(Lights)} and {nameof(LightScales)} must have equal counts.");
+
+        base.Validate();
+    }
+
+    internal override void WriteData(BinaryWriter bw)
+    {
+        bw.WriteP3DString(Name);
+        bw.Write(Version);
+        bw.Write(NumLights);
+        foreach (var light in Lights)
+            bw.WriteP3DString(light);
+        foreach (var lightScale in LightScales)
+            bw.Write(lightScale);
+        bw.Write(NumPhotons);
+        foreach (var photon in Photons)
+            photon.Write(bw);
+    }
+
+    public class Photon
+    {
+        public Vector3 Position { get; set; }
+        public short Plane { get; set; }
+        public byte Theta { get; set; }
+        public byte Phi { get; set; }
+        public float RedPower { get; set; }
+        public float GreenPower { get; set; }
+        public float BluePower { get; set; }
+        public float AlphaPower { get; set; }
+        public byte NumScatterings { get; set; }
+        public byte LightIndex { get; set; }
+
+        public byte[] DataBytes
+        {
+            get
+            {
+                List<byte> data = [];
+
+                data.AddRange(BinaryExtensions.GetBytes(Position));
+                data.AddRange(BitConverter.GetBytes(Plane));
+                data.Add(Theta);
+                data.Add(Phi);
+                data.AddRange(BitConverter.GetBytes(RedPower));
+                data.AddRange(BitConverter.GetBytes(GreenPower));
+                data.AddRange(BitConverter.GetBytes(BluePower));
+                data.AddRange(BitConverter.GetBytes(AlphaPower));
+                data.Add(NumScatterings);
+                data.Add(LightIndex);
+
+                return [.. data];
+            }
+        }
+
+        public Photon(BinaryReader br)
+        {
+            Position = br.ReadVector3();
+            Plane = br.ReadInt16();
+            Theta = br.ReadByte();
+            Phi = br.ReadByte();
+            RedPower = br.ReadSingle();
+            GreenPower = br.ReadSingle();
+            BluePower = br.ReadSingle();
+            AlphaPower = br.ReadSingle();
+            NumScatterings = br.ReadByte();
+            LightIndex = br.ReadByte();
+        }
+
+        public Photon(Vector3 position, short plane, byte theta, byte phi, float redPower, float greenPower, float bluePower, float alphaPower, byte numScatterings, byte lightIndex)
+        {
+            Position = position;
+            Plane = plane;
+            Theta = theta;
+            Phi = phi;
+            RedPower = redPower;
+            GreenPower = greenPower;
+            BluePower = bluePower;
+            AlphaPower = alphaPower;
+            NumScatterings = numScatterings;
+            LightIndex = lightIndex;
+        }
+
+        public Photon()
+        {
+            Position = new();
+            Plane = 0;
+            Theta = 0;
+            Phi = 0;
+            RedPower = 0;
+            GreenPower = 0;
+            BluePower = 0;
+            AlphaPower = 0;
+            NumScatterings = 0;
+            LightIndex = 0;
+        }
+
+        internal void Write(BinaryWriter bw)
+        {
+            bw.Write(Position);
+            bw.Write(Plane);
+            bw.Write(Theta);
+            bw.Write(Phi);
+            bw.Write(RedPower);
+            bw.Write(GreenPower);
+            bw.Write(BluePower);
+            bw.Write(AlphaPower);
+            bw.Write(NumScatterings);
+            bw.Write(LightIndex);
+        }
+
+        public override string ToString()
+        {
+            return $"{Position} | {Plane} | {Theta} | {Phi} | {RedPower} | {GreenPower} | {BluePower} | {AlphaPower} | {NumScatterings} | {LightIndex}";
+        }
+    }
+}
---
+++ NetP3DLib/NetP3DLib/P3D/Chunks/SmartPropChunk.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NetP3DLib.P3D.Chunks;
+
+[ChunkAttributes((uint)ChunkIdentifier.Smart_Prop)]
+public class SmartPropChunk : NamedChunk
+{
+    public uint Version { get; set; }
+    public string ObjectFactoryName { get; set; }
+    public string Material { get; set; }
+    public uint MaterialEnum { get; set; }
+    public uint NumBreakables { get; set; }
+    public uint RenderingCost { get; set; }
+    public uint SimulationCost { get; set; }
+    public uint NumStates { get; set; }
+
+    public override byte[] DataBytes
+    {
+        get
+        {
+            List<byte> data = [];
+
+            data.AddRange(BitConverter.GetBytes(Version));
+            data.AddRange(BinaryExtensions.GetP3DStringBytes(Name));
+            data.AddRange(BinaryExtensions.GetP3DStringBytes(ObjectFactoryName));
+            data.AddRange(BinaryExtensions.GetP3DStringBytes(Material));
+            data.AddRange(BitConverter.GetBytes(MaterialEnum));
+            data.AddRange(BitConverter.GetBytes(NumBreakables));
+            data.AddRange(BitConverter.GetBytes(RenderingCost));
+            data.AddRange(BitConverter.GetBytes(SimulationCost));
+            data.AddRange(BitConverter.GetBytes(NumStates));
+
+            return [.. data];
+        }
+    }
+    public override uint DataLength => sizeof(uint) + (uint)BinaryExtensions.GetP3DStringBytes(Name).Length + (uint)BinaryExtensions.GetP3DStringBytes(ObjectFactoryName).Length + (uint)BinaryExtensions.GetP3DStringBytes(Material).Length + sizeof(uint) + sizeof(uint) + sizeof(uint) + sizeof(uint) + sizeof(uint);
+
+    public SmartPropChunk(BinaryReader br) : base((uint)ChunkIdentifier.Smart_Prop)
+    {
+        Version = br.ReadUInt32();
+        Name = br.ReadP3DString();
+        ObjectFactoryName = br.ReadP3DString();
+        Material = br.ReadP3DString();
+        MaterialEnum = br.ReadUInt32();
+        NumBreakables = br.ReadUInt32();
+        RenderingCost = br.ReadUInt32();
+        SimulationCost = br.ReadUInt32();
+        NumStates = br.ReadUInt32();
+    }
+
+    public SmartPropChunk(uint version, string name, string objectFactoryName, string material, uint materialEnum, uint numBreakables, uint renderingCost, uint simulationCost, uint numStates) : base((uint)ChunkIdentifier.Smart_Prop)
+    {
+        Version = version;
+        Name = name;
+        ObjectFactoryName = objectFactoryName;
+        Material = material;
+        MaterialEnum = materialEnum;
+        NumBreakables = numBreakables;
+        RenderingCost = renderingCost;
+        SimulationCost = simulationCost;
+        NumStates = numStates;
+    }
+
+    public override void Validate()
+    {
+        if (ObjectFactoryName == null)
+            throw new InvalidDataException($"{nameof(ObjectFactoryName)} cannot be null.");
+        if (Encoding.UTF8.GetBytes(ObjectFactoryName).Length > 255)
+            throw new InvalidDataException($"The max length of {nameof(ObjectFactoryName)} is 255 bytes.");
+
+        if (Material == null)
+            throw new InvalidDataException($"{nameof(Material)} cannot be null.");
+        if (Encoding.UTF8.GetBytes(Material).Length > 255)
+            throw new InvalidDataException($"The max length of {nameof(Material)} is 255 bytes.");
+
+        base.Validate();
+    }
+
+    internal override void WriteData(BinaryWriter bw)
+    {
+        bw.Write(Version);
+        bw.WriteP3DString(Name);
+        bw.WriteP3DString(ObjectFactoryName);
+        bw.WriteP3DString(Material);
+        bw.Write(MaterialEnum);
+        bw.Write(NumBreakables);
+        bw.Write(RenderingCost);
+        bw.Write(SimulationCost);
+        bw.Write(NumStates);
+    }
+}
