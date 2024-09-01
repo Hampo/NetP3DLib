8378a945503b3858ebc97268a145121c0bb0a9ca
Proddy <josh@zhbot.org>
2024-09-02 00:02:35 +0100

    Update VertexShaderChunk.cs
    - Changed `VertexShaderName` to just use `NamedChunk`

--- NetP3DLib/NetP3DLib/P3D/Chunks/VertexShaderChunk.cs
+++ NetP3DLib/NetP3DLib/P3D/Chunks/VertexShaderChunk.cs
@@ -1,49 +1,41 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace NetP3DLib.P3D.Chunks;
 
 [ChunkAttributes((uint)ChunkIdentifier.Vertex_Shader)]
-public class VertexShaderChunk : Chunk
+public class VertexShaderChunk : NamedChunk
 {
-    public string VertexShaderName { get; set; }
-
     public override byte[] DataBytes
     {
         get
         {
             List<byte> data = [];
 
-            data.AddRange(BinaryExtensions.GetP3DStringBytes(VertexShaderName));
+            data.AddRange(BinaryExtensions.GetP3DStringBytes(Name));
 
             return [.. data];
         }
     }
-    public override uint DataLength => (uint)BinaryExtensions.GetP3DStringBytes(VertexShaderName).Length;
+    public override uint DataLength => (uint)BinaryExtensions.GetP3DStringBytes(Name).Length;
 
     public VertexShaderChunk(BinaryReader br) : base((uint)ChunkIdentifier.Vertex_Shader)
     {
-        VertexShaderName = br.ReadP3DString();
+        Name = br.ReadP3DString();
     }
 
     public VertexShaderChunk(string vertextShaderName) : base((uint)ChunkIdentifier.Vertex_Shader)
     {
-        VertexShaderName = vertextShaderName;
+        Name = vertextShaderName;
     }
 
     public override void Validate()
     {
-        if (VertexShaderName == null)
-            throw new InvalidDataException($"{nameof(VertexShaderName)} cannot be null.");
-        if (Encoding.UTF8.GetBytes(VertexShaderName).Length > 255)
-            throw new InvalidDataException($"The max length of {nameof(VertexShaderName)} is 255 bytes.");
-
         base.Validate();
     }
 
     internal override void WriteData(BinaryWriter bw)
     {
-        bw.WriteP3DString(VertexShaderName);
+        bw.WriteP3DString(Name);
     }
 }
